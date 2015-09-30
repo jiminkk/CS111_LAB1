1d8f9ad3e4ed1b3bd077f841253106969f1fadae
Jason Xiaoyu Jiang <jasonj@lnxsrv01.seas.ucla.edu>
2015-09-30 01:08:40 -0700

    first commit

---
+++ command-internals.h
@@ -0,0 +1,58 @@
+// UCLA CS 111 Lab 1 command internals
+
+//Since we need the structs a
+
+
+typedef struct linked_list linked_list;
+typedef struct stack stack;
+typedef struct node node;
+
+
+enum command_type
+  {
+    AND_COMMAND,         // A && B
+    SEQUENCE_COMMAND,    // A ; B
+    OR_COMMAND,          // A || B
+    PIPE_COMMAND,        // A | B
+    SIMPLE_COMMAND,      // a simple command
+    SUBSHELL_COMMAND,    // ( A )
+  };
+
+// Data associated with a command.
+struct command
+{
+  enum command_type type;
+
+  // Exit status, or -1 if not known (e.g., because it has not exited yet).
+  int status;
+
+  // I/O redirections, or 0 if none.
+  char *input;
+  char *output;
+
+  union
+  {
+    // for AND_COMMAND, SEQUENCE_COMMAND, OR_COMMAND, PIPE_COMMAND:
+    struct command *command[2];
+
+    // for SIMPLE_COMMAND:
+    char **word;
+
+    // for SUBSHELL_COMMAND:
+    struct command *subshell_command;
+  } u;
+};
+
+struct node {
+	command_t val;
+	node* next;
+};
+
+struct stack{
+	node* top;
+	int num_cmds;
+};
+
+struct linked_list{
+	node* head;
+};
