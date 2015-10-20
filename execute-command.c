498d380f44622bd4c04623fd0d259fa3fc673acb
Jasmine Kim <jasminekim95@yahoo.com>
2015-10-20 11:21:06 -0700

    finished coding

--- execute-command.c
+++ execute-command.c
@@ -24,14 +24,83 @@ execute_command (command_t c, int time_travel)
     int fd[2];
     pid_t child;
     case SIMPLE_COMMAND:
-      //TO DO
-    case SEQUENCE_COMMAND:
-      //TO DO
+      //base case for all recursion
+      child = fork();
 
-      execute_command(c->command[0], time_travel);
-      execute_command(c->command[1], time_travel);
-      c->status = c->command[1]->status;
+      //child process
+      if(child == 0)
+      {
+        //handle redirects here
+        if(c->input)  // <
+        {
+          int input_file;
+
+          //do we need more parameters here?
+          if((input_file = open(c->input, O_RDONLY)) == -1)
+            error(3, 0, "Could not open input file");
+          if(dup2(input_file, read) == -1)
+            error(3, 0, "Read file descriptor for input redirect not found");
+
+          close(input_file);
+        }
+
+        if(c->output) // >
+        {
+          int output_file;
+
+          //do we need more parameters here?
+          if((output_file = open(c->output, O_WRONLY|O_CREAT|O_TRUNC)) == -1)
+            error(3, 0, "Could not write to file");
+          if(dup2(output_file, write) == -1)
+            error(3, 0, "Write file descriptor for output redirect not found");
+
+          close(output_file);
+        }
+        //check if correct
+        if(execvp(c->u.word[0], c->u.word) == -1)
+          error(3, 0, "Cannot execute command");
+      }
+      //parent process
+      else if(child > 0)
+      {
+        int status;
+        waitpid(child, &status, 0); //wait on child process
+        c->status = status; //waitpid returns status of child process
+      }
+      else
+        error(3, 0, "Failed to create child process");
       break;
+
+    case SEQUENCE_COMMAND:
+      //parallelized
+      //should we have to check for dependencies for lab 1b?
+      if(time_travel)
+      {
+        child = fork();
+        //newly created child process
+        if(child == 0)
+        {
+          execute_command(c->u.command[0], time_travel);
+          exit(0);
+        }
+        // parent process
+        if(child > 0)
+        {
+          execute_command(c->u.command[1], time_travel);
+          exit(0);
+        }
+        else
+          error(3, 0, "Child process not created.");
+        waitpid(child, NULL, 0); //wait for child to finish.
+      }
+      else
+      {
+        execute_command(c->u.command[0], time_travel);
+        execute_command(c->u.command[1], time_travel);
+      }
+      c->status = c->u.command[1]->status;
+      break;
+
     case AND_COMMAND:
       execute_command(c->command[0], time_travel);
       c->status = c->command[0]->status;
@@ -43,6 +112,7 @@ execute_command (command_t c, int time_travel)
         c->status = c->command[1]->status;
       }
       break;
+
     case OR_COMMAND:
       execute_command(c->command[0], time_travel);
       c->status = c->command[0]->status;
@@ -54,48 +124,49 @@ execute_command (command_t c, int time_travel)
         c->status = c->command[1]->status;
       }
       break;
+
     case PIPE_COMMAND:
       int child_status;
       pid_t first_pid, second_pid, return_pid;
       int mypipe[2];
-      pipe(mypipe); //assuming its function that must be defined later 
+      pipe(mypipe);
       first_pid = fork();
+
       if (first_pid == 0) //child process
       {
-        close(mypipe[0];
-        
-        
+        close(mypipe[read];
+
         ///NOTE There may be a logic error depending on order of closing and executing commands
         
-        
-        //Chek for error with pipe
+        //Check for error with duplication
         if (dup2(mypipe[write], write);
         {
+          execute_command(c->u.command[0], time_travel);   //execute_switch(c->u.command[0]);
+          c->status = c->u.command[0]->status;
           close(mypipe[write]);
-        } 
+          exit(0);
+        }
         else
         {
-          error(3, 0, "Failure to close mypipe[write]");
+          error(3, 0, "Failure to duplicate write file descriptor");
         }
-        execute_command(c->u.command[read])   //execute_switch(c->u.command[0]);
       }
-      else if (first_pid > 0)
+
+      else if (first_pid > 0) //parent process
       {
-        
         waitpid(first_pid, &child_status, 0);
         
         close(mypipe[write]);
         
         if (dup2(mypipe[read],read)
         { 
-          close(mypipe[read],read);
+          execute_command(c->command[1], time_travel);
+          c->status = c->u.command[1]->status;
+          close(mypipe[read]);
+          exit(0);
         }
         else
-          error(3, 0, "Failure to close mypipe[read]");
-          
-        execute_command(c->command[1], time_travel);
-        c->status = c->command[1]->status;
-        close(mypipe[read]);
+          error(3, 0, "ailure to duplicate read file descriptor");
       }
       else
           error(3, 0, "Failed to create child process");
